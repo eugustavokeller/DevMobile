66819c6f67e38decfab71e92d892e05f44714cdc
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-07-10 17:36:43 -0300

    Create desafio if_else

---
+++ curso_dart/controle/desafio_if_else.dart
@@ -0,0 +1,11 @@
+main() {
+  var nota = 2.3;
+
+  if (nota >=
+      9.0) // ; (ao usar ponto e virgula o primeiro código executado pelo if é vazio e consequentemente o print será executado!)
+  {
+    print('Parabéns! Vc foi brilhante!');
+  }
+}
+
+// Não se usa ; em estrutura de controle, com exceção DO/WHILE!!!
