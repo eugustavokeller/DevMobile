a17f30d1c45b6156a649125b33af91940e0a7e89
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-07-11 19:00:06 -0300

    Create For

---
+++ curso_dart/controle/for_1.dart
@@ -0,0 +1,15 @@
+main() {
+
+  for(int a = 0; a < 10; a++) {
+    print('a = $a');
+  }
+  print('Fim!!');
+
+  for(int b = 100; b >= 0; b -= 4) {
+  print('b = $b');
+  }
+  print('Fim 2!!');
+
+}
+
+
