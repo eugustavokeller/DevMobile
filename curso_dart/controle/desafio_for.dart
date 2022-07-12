7561360882e16b8d1c890c8a268b501054678caa
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-07-12 14:17:52 -0300

    Update desafio_for.dart

--- curso_dart/controle/desafio_for.dart
+++ curso_dart/controle/desafio_for.dart
@@ -12,12 +12,15 @@ Use o laço FOR, mas não pode controlar o laço usando o valor númerico */
 
 main() {
 
+  // Forma que não pode ser usada...
   var valor = '#';
   for(int i = 0; i < 6; i++) {
     print(valor);
     valor += '#';
   }
 
+
+  // Forma que deve ser usada sem números para controlar laço...
   for(var valor = '#'; valor != '#######'; valor += '#') {
     print(valor);
   }
