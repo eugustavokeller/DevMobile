afdffe91d551a3898f2647924023a2cf33d8c626
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-07-13 12:41:58 -0300

    Create funcao em variavel 2

---
+++ curso_dart/funcao/funcao_em_variavel_2.dart
@@ -0,0 +1,13 @@
+main() {
+  var adicao = (int a, int b) => a + b;
+  var subtracao = (int a, int b) => a - b;
+  var multiplicacao = (int a, int b) => a * b;
+  var divisao = (int a, int b) => a / b;
+  
+  
+  print(adicao(4, 5));
+  print(subtracao(9, 5));
+  print(multiplicacao(4, 5));
+  print(divisao(16, 2));
+}
+ 
