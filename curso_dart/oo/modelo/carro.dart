90004d0544a15925e8612251e25c3b69d8371f7f
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-07-18 17:26:23 -0300

    Update Carro

--- curso_dart/oo/modelo/carro.dart
+++ curso_dart/oo/modelo/carro.dart
@@ -3,29 +3,28 @@ class Carro {
   final int velocidadeMax = 280;
   int velocidadeAtual = 0;
 
-}
-
-acelerar(velocidadeAtual, velocidadeMax) {
+  acelerar() {
     print('A velocidade antes de acelerar é: $velocidadeAtual.');
     while(velocidadeAtual < velocidadeMax) {
       return velocidadeAtual += 5;
     }
     print('Após acelerar é: $velocidadeAtual.');
-}
+  }
   
-estaNoLimite(velocidadeMax) {
+  estaNoLimite(velocidadeMax) {
     print('Chegou ao limite da velocidade!!');
     return frear;
-}
+  }
 
-frear(velocidadeAtual, velocidadeMax) {
-  print('A velocidade antes de frear é: $velocidadeAtual.');
-  if(velocidadeAtual >= velocidadeMax) {
-    print(estaNoLimite(velocidadeMax));
-    velocidadeAtual -= 5;
-    print('Velocidade após frear');
+  frear(velocidadeAtual, velocidadeMax) {
+    print('A velocidade antes de frear é: $velocidadeAtual.');
+    if(velocidadeAtual >= velocidadeMax) {
+      print(estaNoLimite(velocidadeMax));
+      velocidadeAtual -= 5;
+      print('Velocidade após frear');
     } else if(velocidadeAtual == 0) {
       print('Carro parou!!');
-    }
-  
+      }
+  }
+
 }
