58c1f750ec3c3c4c015dc949b674772b34b42af3
PedroRav <85264199+eugustavokeller@users.noreply.github.com>
2022-07-27 12:39:50 -0300

    Update Callback

--- curso_flutter/projeto_perguntas/lib/resposta.dart
+++ curso_flutter/projeto_perguntas/lib/resposta.dart
@@ -2,16 +2,21 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
-
   final String resposta;
+  final void Function() quandoSelecionado;
 
-  Resposta(this.resposta);
+  Resposta(this.resposta, this.quandoSelecionado);
   
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text(resposta),
-      onPressed: null,
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        textColor: Colors.white,
+        color: Colors.blue,
+        child: Text(resposta),
+        onPressed: quandoSelecionado,
+      ),
     );
   }  
 }
